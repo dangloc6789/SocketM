76601295c5a55d793882456ce2f6d899084b44df
dangloc6789 <83728109+dangloc6789@users.noreply.github.com>
2023-08-13 19:04:44 +0700

    sadasdf

--- deliver/customer/models.py
+++ deliver/customer/models.py
@@ -25,6 +25,7 @@ class OrderModel(models.Model):
     city =models.CharField(max_length=50, blank=True)
     district=models.CharField(max_length=20, blank=True)
     zip_code=models.IntegerField(blank=True,null=True)
+    is_paid=models.BooleanField(default=False)
     
     def __str__(self):
         return f'Order:{self.create_on.strftime("%b %d %I: %M %p")}'
